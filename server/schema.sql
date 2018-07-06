37505be1602e2c48bc67104bd9f9974226fa940a
JKAnderson409 <JKAnderson409@gmail.com>
2018-07-06 19:59:28 -0400

    Made the tables in chat database

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,31 @@
+/* delete db cpmmand goes */
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  
-  /* Describe your table here.*/
+
+  id INTEGER PRIMARY KEY,
+  user TEXT NOT NULL,
+  room INTEGER NOT NULL,
+  msg TEXT NOT NULL,
+  timesent TIMESTAMP NOT NULL
+);
+
+CREATE TABLE users (
+
+  id INTEGER PRIMARY KEY,
+  userName TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+
+  msgId INTEGER NOT NULL,
+  userId INTEGER NOT NULL,
+  FOREIGN KEY (msgId) REFERENCES messages(id),
+  FOREIGN KEY (userId) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
